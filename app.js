1ca0b2058d5016da01d69fe71705d1b8be3c5b71
matthieuEv <evainmatthieu44@gmail.com>
2023-08-24 16:57:29 +0200

    patch: Refactor search output

--- app.js
+++ app.js
@@ -5,16 +5,16 @@ require('dotenv').config();
 ZTP._devMode = true // Enable dev mode (more logs)
 ZTP.setMoviesDbToken(process.env.MOVIESDB_API_KEY);
 
-let category = "series" // The category you want to search / list of categories at ZTP._allCategories
+let category = "films" // The category you want to search / list of categories at ZTP._allCategories
 // Currently, only "films" is tested, so it may crash or not work properly for other categories
-let query = "Mercredi" // Your search query
+let query = "interstellar" // Your search query
 
 async function __main__() {
     let response1 = await ZTP.search(category, query)
 
     console.log("Search: ", response1,"\n")
     // console.log("SearchAll: ", await ZTP.searchAll(category, query),"\n")
-    console.log("\n\getMovieDatas: ", await ZTP.getMovieDatas(category, Object.values(response1)[0][0].id),"\n")
+    console.log("\n\getMovieDatas: ", await ZTP.getMovieDatas(category, response1[0].id),"\n")
 }
 
 __main__()
