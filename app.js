5855b94cb683ccdb6cf1db9b78497be8c4db192b
matthieuEv <evainmatthieu44@gmail.com>
2023-08-27 14:32:30 +0200

    fix: change file location & fix issues with getQueryDatas()

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ let query = "mercredi" // Your search query
 async function __main__() {
     let response1 = await ZTP.search(category, query)
 
-    console.log("Search: ", response1,"\n")
+    console.log("Search: ", response1[0],"\n")
     // console.log("SearchAll: ", await ZTP.searchAll(category, query),"\n")
     console.log("\n\getQueryDatas: ", await ZTP.getQueryDatas(category, response1[0].id),"\n")
 }
